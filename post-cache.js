32d39b71c59d1c267713ee7e74e3d3dbfb284df6
phil <uniphil@gmail.com>
2024-10-01 21:41:48 -0400

    sqlite: well the tests pass, runs in mem at least

--- post-cache.js
+++ post-cache.js
@@ -1,42 +1,96 @@
+import Database from 'better-sqlite3';
 
 const DEFAULT_MAX_ITEMS = 1000000;
 const DEFAULT_MAX_AGE_MS = 24 * 60 * 60 * 1000;
+const DEFAULT_SQLITE_DB = ':memory:';
+
+
 
 class PostCache {
-  #map;
   #maxItems;
   #maxAge;
+
+  #db;
+  #dbSet;
+  #dbUpdate;
+  #dbTake;
+  #dbTrim;
+  #dbCount;
+  #dbOldest;
 
   constructor({
     maxItems = DEFAULT_MAX_ITEMS,
     maxAge = DEFAULT_MAX_AGE_MS,
+    sqliteDb = DEFAULT_SQLITE_DB,
   } = {}) {
-    this.#map = new Map();
     this.#maxItems = maxItems;
     this.#maxAge = maxAge;
+    this.#db = new Database(sqliteDb, { timeout: 1000 });
+    // thanks https://kerkour.com/sqlite-for-servers
+    this.#db.pragma('journal_mode = WAL');
+    this.#db.pragma('synchronous = NORMAL');
+    this.#db.pragma(`cache_size = ${100 * Math.pow(2, 20)}`); // 100MiB
+    this.#db.exec(`
+      create table if not exists posts (
+        rkey text    primary key,
+        t    integer not null, -- not (necessarily) a unix timestamp!
+        data text    not null -- json blob
+      ) strict;
+      create index if not exists posts_t on posts (t);
+    `);
+
+    this.#dbSet = this.#db.prepare(
+      'insert into posts values (?, ?, ?)');
+
+    this.#dbUpdate = this.#db.prepare(
+      'update posts set data = ? where rkey = ?');
+
+    this.#dbTake = this.#db.prepare(
+      'delete from posts where rkey = ? returning t, data');
+
+    this.#dbTrim = this.#db.prepare(`
+      with cutoff as (
+        select rowid
+          from posts
+         order by rowid desc -- should work for a pretty long time
+         limit 1
+        offset ?
+      )
+      delete from posts
+      where t < ? or rowid <= (select rowid from cutoff)
+    `);
+
+    this.#dbCount = this.#db.prepare(
+      'select count(rowid) as count from posts');
+
+    this.#dbOldest = this.#db.prepare(
+      'select t from posts order by rowid limit 1');
   }
 
   set(now, k, v) {
-    this.#map.set(k, { v, t: now });
+    this.#dbSet.run(k, now, JSON.stringify(v));
     this.#cleanup(now);
   }
 
   update(now, k, v) {
-    const found = this.#map.get(k);
-    if (found !== undefined) {
-      found.v = v;
-    }
+    this.#dbUpdate.run(JSON.stringify(v), k);
     this.#cleanup(now);
   }
 
   take(now, k) {
     let rv;
-    let rec = this.#map.get(k);
+    let rec = this.#dbTake.get(k);
     if (rec !== undefined) {
-      this.#map.delete(k);
       if (rec.t >= now - this.#maxAge) {
+        let value;
+        try {
+          value = JSON.parse(rec.data);
+        } catch (e) {
+          console.error('failed to parse json data from cache db', rec);
+          return;
+        }
         rv = {
-          value: rec.v,
+          value: JSON.parse(rec.data),
           age: now - rec.t,
         };
       }
@@ -46,23 +100,15 @@ class PostCache {
   }
 
   size() {
-    return this.#map.size;
+    return this.#dbCount.get()?.count;
   }
 
   oldest() {
-    return this.#map.values().next().value?.t;
+    return this.#dbOldest.get()?.t;
   }
 
   #cleanup(now) {
-    let sizeOver = this.#map.size - this.#maxItems;
-    const minTime = now - this.#maxAge;
-    const toDelete = [];
-    for (const [k, { t }] of this.#map) {
-      if (sizeOver <= 0 && t >= minTime) break;
-      sizeOver -= 1;
-      toDelete.push(k);
-    }
-    toDelete.forEach(k => this.#map.delete(k));
+    this.#dbTrim.run(this.#maxItems, now - this.#maxAge);
   }
 }
 
